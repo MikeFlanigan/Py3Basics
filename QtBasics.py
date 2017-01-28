31a63e5035991d628f0260d9f209c6d91626577a
mike <flanman1866@gmail.com>
2017-01-28 00:53:46 -0500

    begginnings of learning python qt

---
+++ QtBasics.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt4 import QtGui
+
+
+## 3 major sections
+
+# 1 app def
+app = QtGui.QApplication(sys.argv) #  sys.argv accepts command line arguments
+
+window = Qt.Gui.QWidget() # ad 
+
+window.show() # graphics made in background and then displayed
